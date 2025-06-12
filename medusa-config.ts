a99eb5f022d652704998465bca2c1d807ee1cc26
0xsarwagya <contact@0xsarwagya.codes>
2025-06-12 22:53:38 +0200

    base: initial commit

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,10 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
+import type { AuthOptions } from 'medusa-plugin-auth'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+const BACKEND_URL = process.env.BACKEND_URL || "localhost:9000"
+const ADMIN_URL = process.env.ADMIN_URL || "localhost:7000"
+const STORE_URL = process.env.STORE_URL || "localhost:8000"
 
 module.exports = defineConfig({
   projectConfig: {
@@ -77,6 +81,6 @@ module.exports = defineConfig({
           },
         ],
       },
-    }
+    },
   ],
 })
