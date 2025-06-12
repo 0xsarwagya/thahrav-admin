2015ec7f71d884e6209997994b79430550a05085
0xsarwagya <contact@0xsarwagya.codes>
2025-06-12 04:45:20 +0200

    base: initial commit

--- src/jobs/keep-alive.ts
+++ src/jobs/keep-alive.ts
@@ -1,6 +1,6 @@
 import { MedusaContainer } from "@medusajs/framework/types"
 
-export default async function greetingJob(container: MedusaContainer) {
+export default async function keepAliveJob(container: MedusaContainer) {
     const response = await fetch("https://admin.thahrav.shop/")
     const logger = container.resolve('logger')
     logger.info("Keep-alive!")
